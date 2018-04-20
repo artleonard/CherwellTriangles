5ef9ce4b9235c320effc03efb4da700f43ace1f5
artleonard <38524297+artleonard@users.noreply.github.com>
2018-04-19 20:59:45 -0400

    Add files via upload

---
+++ Controllers/TrianglesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CherwellTriangles.Models;
+
+namespace CherwellTriangles.Controllers
+{
+    public class TrianglesController : ApiController
+    {
+        // GET: api/Triangle
+        public Triangle GetTriangleCoordinates(string row, int column)
+        {
+            return new Triangle(new RowColumn(row, column));
+        }
+
+        // GET: api/TrianglePosition
+        public RowColumn GetTrianglePosition(int V1x, int V1y, int V2x, int V2y, int V3x, int V3y)
+        {
+            Triangle t = new Triangle(V1x, V1y, V2x, V2y, V3x, V3y);
+            return t.GetRowColumn;
+        }
+    }
+}
