cf9898f98c456cec6ccaf6a5b99f8b554520ba69
artleonard <38524297+artleonard@users.noreply.github.com>
2018-04-19 20:59:56 -0400

    Add files via upload

---
+++ Models/RowColumn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CherwellTriangles.Models
+{
+    public class RowColumn
+    {
+        private string mRow;
+        private int mColumn;
+
+        public RowColumn(string pRow, int pColumn)
+        {
+            this.mRow = pRow;
+            this.mColumn = pColumn;
+        }
+
+        public string Row
+        {
+            get { return mRow; }
+            set { mRow = value; }
+        }
+
+        public int Column
+        {
+            get { return mColumn; }
+            set { mColumn = value; }
+        }
+
+        public override string ToString()
+        {
+            return mRow + mColumn.ToString();
+        }
+    }
+}
